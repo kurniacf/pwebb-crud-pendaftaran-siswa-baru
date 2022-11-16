6e12a3dff87640ea53a69c581b98cbb1bc8d4cc7
Kurnia Cahya Febryanto <kcf170202@gmail.com>
2022-11-17 05:34:43 +0700

    feat: added crud system

--- config.php
+++ config.php
@@ -10,4 +10,4 @@ $db = mysqli_connect($server, $user, $password, $nama_database);
 if (!$db) {
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
-echo "Berhasil terhubung dengan database";
+//echo "Berhasil terhubung dengan database";
